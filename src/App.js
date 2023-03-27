557c8f8f0b7e4597938c15e7574e06180d3c46f3
shemaamohamed <shimaaeldakhakhny@gmail.com>
2023-03-27 03:26:08 +0200

    gfdd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import {Header} from './components/Shared/Header';
 import { Footer } from './components/Shared/Footer';
+import {Update}from "./components/pages/UserPages/Update";
 
 
 function App() {
   return (
     <div >
       <Header/>
+      <Update/>
       <Footer/>
       
     </div>
