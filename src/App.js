409b2a633b2ede1a3b740f6c750e53a8587d9e6e
shemaamohamed <shimaaeldakhakhny@gmail.com>
2023-03-27 17:00:42 +0200

    aaa

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {Header} from './components/Shared/Header';
 import { Footer } from './components/Shared/Footer';
 import {Update}from "./components/pages/UserPages/Update";
 import {Register}from "./components/pages/Login/Register"
+import { SideAdmin } from './components/Shared/SideAdmin';
+import "./components/Shared/SharedStyle/App.css"
+
 
 
 function App() {
   return (
-    <div >
+    <div className='app' >
       <Header/>
-      <Register/>
+      <SideAdmin/>
       <Footer/>
       
     </div>
---
+++ src/components/Shared/SideAdmin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Shared/SharedStyle/SideAdmin.css"
+
+export const SideAdmin = () => {
+  return (
+    <div className='side'>
+        <button >ManageQuestion</button>
+        <button>ManaageUsers</button>
+        <button>Registeration approvement</button>
+
+    </div>
+  )
+}
