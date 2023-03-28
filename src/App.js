d63d3788e9366c715928ce6193d9c46000ed0fd8
Ahmed Mostafa <ahmed.mostafa8582@gmail.com>
2023-03-28 02:36:22 +0200

    simple edit for errors

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import { Footer } from './components/Shared/Footer';
 import {Update}from "./components/pages/UserPages/Update";
 import {Register}from "./components/pages/Login/Register"
 import { SideAdmin } from './components/Shared/SideAdmin';
+import  Home  from './components/pages/UserPages/Home';
 import "./components/Shared/SharedStyle/App.css"
+import NavBar from './components/Shared/NavBar';
 
 
 
 function App() {
   return (
     <div className='app' >
+<<<<<<< Updated upstream
       <Header/>
 
       <Footer/>
+=======
+      {/* <Header/> */}
+      <NavBar></NavBar>
+      <Home></Home>
+      {/* <Footer/> */}
+>>>>>>> Stashed changes
       
     </div>
   );
