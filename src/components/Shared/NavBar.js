c69e5c042e6fbfdc3cf7f898720de925f80347a0
shemaamohamed <shimaaeldakhakhny@gmail.com>
2023-03-28 20:21:11 +0200

    ear logo

--- src/components/Shared/NavBar.js
+++ src/components/Shared/NavBar.js
@@ -28,7 +28,7 @@ const NavBar = () => {
     <div className='container'>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src='logo1.png'></img></a>
+    <a  href="#"><img class="navbar-brand" src='logo1.png'></img></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
