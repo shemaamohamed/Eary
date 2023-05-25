8aedf717becd06ab9ce3f64396fce8a011e98d4e
Ahmed Mostafa Shawky <98565831+almomua@users.noreply.github.com>
2023-05-26 00:49:38 +0300

    last database

--- Server/DataBase/eary.sql
+++ Server/DataBase/eary.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 06, 2023 at 03:03 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2023 at 11:34 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,164 +26,42 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `exam`
 --
-
-CREATE TABLE `exam` (
-  `id` varchar(50) NOT NULL,
-  `Name` varchar(200) NOT NULL,
-  `number_of_questions` int(11) DEFAULT 0,
-  `Description` text DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Error reading structure for table eary.exam: #1932 - Table &#039;eary.exam&#039; doesn&#039;t exist in engine
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `exam_question`
 --
-
-CREATE TABLE `exam_question` (
-  `exam_id` varchar(50) NOT NULL,
-  `question_id` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Error reading structure for table eary.exam_question: #1932 - Table &#039;eary.exam_question&#039; doesn&#039;t exist in engine
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `history`
 --
-
-CREATE TABLE `history` (
-  `user_id` int(11) NOT NULL,
-  `exam_id` varchar(50) NOT NULL,
-  `question_id` varchar(50) NOT NULL,
-  `Answer` varchar(200) DEFAULT NULL,
-  `IsRight` tinyint(1) NOT NULL DEFAULT 0,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Error reading structure for table eary.history: #1932 - Table &#039;eary.history&#039; doesn&#039;t exist in engine
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `password_resets`
 --
-
-CREATE TABLE `password_resets` (
-  `user_email` varchar(50) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Error reading structure for table eary.password_resets: #1932 - Table &#039;eary.password_resets&#039; doesn&#039;t exist in engine
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `questions`
 --
-
-CREATE TABLE `questions` (
-  `id` varchar(50) NOT NULL,
-  `Name` varchar(200) NOT NULL,
-  `Audio` varchar(500) NOT NULL,
-  `RightAnswer` varchar(200) NOT NULL,
-  `Wrong1` varchar(200) NOT NULL,
-  `Wrong2` varchar(200) NOT NULL,
-  `Wrong3` varchar(200) NOT NULL,
-  `Description` text DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Error reading structure for table eary.questions: #1932 - Table &#039;eary.questions&#039; doesn&#039;t exist in engine
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `password` text NOT NULL,
-  `image` varchar(500) DEFAULT NULL,
-  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
-  `is_verified` tinyint(1) NOT NULL DEFAULT 0,
-  `is_accepted` tinyint(1) NOT NULL DEFAULT 0,
-  `token` varchar(255) DEFAULT NULL,
-  `last_login` datetime DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `exam`
---
-ALTER TABLE `exam`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Name` (`Name`);
-
---
--- Indexes for table `exam_question`
---
-ALTER TABLE `exam_question`
-  ADD PRIMARY KEY (`exam_id`,`question_id`),
-  ADD KEY `question_id_constr` (`question_id`);
-
---
--- Indexes for table `history`
---
-ALTER TABLE `history`
-  ADD PRIMARY KEY (`user_id`,`exam_id`,`question_id`,`created_at`),
-  ADD KEY `history_exam_id_constr` (`exam_id`),
-  ADD KEY `history_question_id_constr` (`question_id`);
-
---
--- Indexes for table `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Audio` (`Audio`),
-  ADD UNIQUE KEY `Name` (`Name`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Email` (`email`),
-  ADD UNIQUE KEY `name` (`name`) USING BTREE,
-  ADD UNIQUE KEY `phone` (`phone`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `exam_question`
---
-ALTER TABLE `exam_question`
-  ADD CONSTRAINT `exam_id_constr` FOREIGN KEY (`exam_id`) REFERENCES `exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `question_id_constr` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `history`
---
-ALTER TABLE `history`
-  ADD CONSTRAINT `history_exam_id_constr` FOREIGN KEY (`exam_id`) REFERENCES `exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `history_question_id_constr` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `history_user_id_constr` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- Error reading structure for table eary.users: #1932 - Table &#039;eary.users&#039; doesn&#039;t exist in engine
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
